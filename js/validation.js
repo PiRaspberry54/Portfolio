0905773f2d6205634f72a6ccac8af778d52caa63
Robin McKenzie <robinmckenzie2@gmail.com>
2022-03-26 07:53:03 +0000

    Tidied up files and refactored code out into functions

--- js/validation.js
+++ js/validation.js
@@ -1,37 +1,51 @@
-$('#submit-contact-form-button').on('click', function() {
-    var wholeFormValid = true;
-    wholeFormValid = ValidateRequiredField(firstname, firstname_required_text) && wholeFormValid;
-    wholeFormValid = ValidateRequiredField(surname, surname_required_text) && wholeFormValid;
-    if(wholeFormValid) {
-        FadeThankYouInAndOut();
-    }
-});
+/*
+Validation functions
 
-function FadeThankYouInAndOut() {
-    $("#form-submitted-thank-you")
-        .hide()
-        .fadeIn(2000)
-        .delay(3000)
-        .fadeOut(1000);
+*/
+
+BindValidationToSubmitButton();
+
+function BindValidationToSubmitButton() {
+    $('#submit-contact-form-button').on('click', function() {
+        var wholeFormValid = true;
+        wholeFormValid = ValidateRequiredField(firstname, firstname_required_text) && wholeFormValid;
+        wholeFormValid = ValidateRequiredField(surname, surname_required_text) && wholeFormValid;
+        wholeFormValid = ValidateRequiredField(surname, surname_required_text) && wholeFormValid;
+        
+        if(wholeFormValid) {
+            FadeThankYouInAndOut();
+        }
+    });
 }
-// Validates that the field passed is not blank,
-// and shows / hides "required" text.
-// and other styling as appropriate
-// Returns:
-// true if valid
-// falsse if invalid
-function ValidateRequiredField(fieldId, requiredTextId) {
-    if(IsBlank(fieldId)){
+
+/* Validates that the field passed is not blank.
+    Shows / hides "required" text
+    and applies / removes other styling as appropriate.
+    Inputs:
+        fieldId         The ID of the element to validate
+        requiredTextId  The ID of the element to show if invalid / hide if valid 
+    Returns:
+        true  if   valid
+        false if invalid
+*/
+
+function ValidateRequiredField(fieldId, requiredTextId, regEx) {
+    if(IsBlank(fieldId) || !ConformsToRegex(fieldId)) {
         ShowRequiredTextAndStyle(fieldId, requiredTextId);
         return false;
     }
+
     HideRequiredTextAndStyle(fieldId, requiredTextId);
     return true;
 }
 
-function IsBlank(fieldId){
+function IsBlank(fieldId) {
     var fieldValue = $(fieldId).val();
     return fieldValue == '' ? true : false;
+}
+
+function ConformsToRegex(fieldId) {
+    return true;
 }
 
 function ShowRequiredTextAndStyle(fieldId, requiredTextId){
@@ -42,4 +56,12 @@ function ShowRequiredTextAndStyle(fieldId, requiredTextId){
 function HideRequiredTextAndStyle(fieldId, requiredTextId){
     $(requiredTextId).hide();
     $(fieldId).css("border", "none");
+}
+
+function FadeThankYouInAndOut() {
+    $("#form-submitted-thank-you")
+        .hide()
+        .fadeIn(2000)
+        .delay(3000)
+        .fadeOut(1000);
 }
