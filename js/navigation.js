1cdf4275b6d42566d995953caf38c9851211551c
Jasmine <Jasmine@Jasmines-MacBook-Pro.local>
2022-07-04 14:47:27 +0100

    Overlay navigation added but not working properly on main file

--- js/navigation.js
+++ js/navigation.js
@@ -1,15 +1,15 @@
-const menuButton = $(".menu-btn");
-const exitButton = $(".exit-button")
-$(".top-nav").css("display", "none");
+// const menuButton = $(".menu-btn");
+// const exitButton = $(".exit-button")
+// $(".top-nav").css("display", "none");
 
-menuButton.on("click", function() {
-    $(".top-nav").css("display", "flex");
-    console.log("Menu button has been pressed.");
-    menuButton.css("display", "none");
-});
+// menuButton.on("click", function() {
+//     $(".top-nav").css("display", "flex");
+//     console.log("Menu button has been pressed.");
+//     menuButton.css("display", "none");
+// });
 
-exitButton.on("click", function() {
-    $(".top-nav").css("display", "none");
-    console.log("Exit button has been pressed.");
-    menuButton.css("display", "flex");
-});
+// exitButton.on("click", function() {
+//     $(".top-nav").css("display", "none");
+//     console.log("Exit button has been pressed.");
+//     menuButton.css("display", "flex");
+// });
